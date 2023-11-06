5f411e60b0ed99727a15cbcad7deece5b8cfa2ce
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-11-06 14:33:03 -0300

    Ajuste de carregamento do Mapper

--- src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapper.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapper.java
@@ -3,8 +3,10 @@ package guru.sfg.beer.order.service.web.mappers;
 import guru.sfg.beer.order.service.domain.BeerOrderLine;
 import guru.sfg.beer.order.service.web.model.BeerOrderLineDto;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper(uses = {DateMapper.class})
+@Component
 public interface BeerOrderLineMapper {
     BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line);
 
--- src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
@@ -3,9 +3,11 @@ package guru.sfg.beer.order.service.web.mappers;
 import guru.sfg.beer.order.service.domain.BeerOrder;
 import guru.sfg.beer.order.service.web.model.BeerOrderDto;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 
 @Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
+@Component
 public interface BeerOrderMapper {
 
     BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
