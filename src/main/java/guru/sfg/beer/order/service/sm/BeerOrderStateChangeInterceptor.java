e1e74fe1ae5304c44e1a256f44bd3f0d25948143
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-11-17 18:00:50 -0300

    Após Tarefa 06 (227) - Criar o OrderState Interceptor - Refactor

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateChangeInterceptor.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateChangeInterceptor.java
@@ -1,9 +1,10 @@
-package guru.sfg.beer.order.service.services;
+package guru.sfg.beer.order.service.sm;
 
 import guru.sfg.beer.order.service.domain.BeerOrder;
 import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
 import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
 import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.services.BeerOrderManagerImpl;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
